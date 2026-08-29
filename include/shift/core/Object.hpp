f33cffcf46fd6af6f4ecc84c5a9978886a0f5ca6
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-08-28 22:12:25 -0400

    feat: add Components to Object

--- include/shift/core/Object.hpp
+++ include/shift/core/Object.hpp
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <memory>
+#include <span>
 #include <string>
 #include <string_view>
+#include <vector>
 
+#include "shift/core/Component.hpp"
 #include "shift/core/UUID.hpp"
 #include "shift/core/shift_core_export.hpp"
 
@@ -27,9 +31,32 @@ public:
 
     auto name() const -> std::string_view { return m_name; }
 
+    auto components() const -> std::span<const std::unique_ptr<Component>>
+    {
+        return m_components;
+    }
+
+    auto add_component(std::unique_ptr<Component> component) -> bool;
+
+    template<typename T, typename... Args>
+        requires std::is_base_of_v<Component, T>
+    auto add_component(Args&&... args) -> bool
+    {
+        return add_component(std::make_unique<T>(std::forward<Args>(args)...));
+    }
+
+    auto has_component(UUID uuid) const -> bool;
+
+    auto has_component(std::string_view name) const -> bool;
+
+    auto get_component(UUID uuid) const -> Component*;
+
+    auto get_component(std::string_view name) const -> Component*;
+
 private:
     SHIFT_SUPPRESS_C4251
     std::string m_name;
+    std::vector<std::unique_ptr<Component>> m_components;
     UUID m_uuid{UUID::build()};
 };
 
--- source/core/Object.cpp
+++ source/core/Object.cpp
@@ -1,8 +1,13 @@
+#include <algorithm>
+#include <memory>
+#include <ranges>
 #include <string>
+#include <string_view>
 #include <utility>
 
 #include "shift/core/Object.hpp"
 
+#include "shift/core/Component.hpp"
 #include "shift/core/UUID.hpp"
 
 namespace shift
@@ -16,6 +21,57 @@ auto Object::uuid() const -> UUID
 auto Object::set_name(std::string name) -> void
 {
     m_name = std::move(name);
+}
+
+auto Object::add_component(std::unique_ptr<Component> component) -> bool
+{
+    if (!component) {
+        return false;
+    }
+
+    // todo: enforce uniqueness?
+    m_components.push_back(std::move(component));
+    return true;
+}
+
+auto Object::has_component(UUID uuid) const -> bool
+{
+    return get_component(uuid) != nullptr;
+}
+
+auto Object::has_component(std::string_view name) const -> bool
+{
+    return get_component(name) != nullptr;
+}
+
+auto Object::get_component(UUID uuid) const -> Component*
+{
+    auto pipeline = m_components
+        | std::views::transform([](const std::unique_ptr<Component>& component)
+                                { return component.get(); })
+        | std::views::filter([](const Component* component)
+                             { return component != nullptr; });
+
+    const auto found =
+        std::ranges::find_if(pipeline,
+                             [&uuid](const Component* component)
+                             { return component->uuid() == uuid; });
+    return found != pipeline.end() ? *found : nullptr;
+}
+
+auto Object::get_component(std::string_view name) const -> Component*
+{
+    auto pipeline = m_components
+        | std::views::transform([](const std::unique_ptr<Component>& component)
+                                { return component.get(); })
+        | std::views::filter([](const Component* component)
+                             { return component != nullptr; });
+
+    const auto found =
+        std::ranges::find_if(pipeline,
+                             [&name](const Component* component)
+                             { return component->name() == name; });
+    return found != pipeline.end() ? *found : nullptr;
 }
 
 }  // namespace shift
