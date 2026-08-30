dd4746533952d180d39ef95689e6cdd19184516a
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-08-30 15:11:27 -0400

    feat: assert runtime constraints

--- source/core/SimulationRunner.cpp
+++ source/core/SimulationRunner.cpp
@@ -14,13 +14,16 @@ namespace shift
 SimulationRunner::SimulationRunner(std::unique_ptr<TimeUpdater> updater)
     : m_updater{std::move(updater)}
 {
-    // todo: check for null updater?
+    if (m_updater == nullptr) {
+        throw std::runtime_error(
+            "SimulationRunner must have valid TimeUpdater");
+    }
 }
 
 auto SimulationRunner::run(Simulation& simulation) -> void
 {
     if (m_updater == nullptr) {
-        throw std::runtime_error("cannot run simulation without updater");
+        throw std::runtime_error("cannot run Simulation without TimeUpdater");
     }
 
     // todo: run until end condition is met
