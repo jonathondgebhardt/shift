b835108c8101173f97c52ec593f805141200cdba
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-08-30 15:53:15 -0400

    feat: add World to Simulation

--- include/shift/core/Simulation.hpp
+++ include/shift/core/Simulation.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "shift/core/Clock.hpp"
+#include "shift/core/World.hpp"
 #include "shift/core/shift_core_export.hpp"
 
 namespace shift
@@ -11,9 +12,11 @@ class SHIFT_CORE_EXPORT Simulation
 public:
     auto clock() -> Clock& { return m_clock; }
 
+    auto world() -> World& { return m_world; }
+
 private:
     Clock m_clock;
-    // world
+    World m_world;
     // systems
     // services
 };
