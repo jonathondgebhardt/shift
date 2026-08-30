26d8ed888075d4b652bc4fb8bcc880ae7b4ef1ba
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-08-30 07:14:51 -0400

    feat: use TimeUpdater to determine next time

--- source/core/Clock.cpp
+++ source/core/Clock.cpp
@@ -1,11 +1,51 @@
+#include <algorithm>
+#include <memory>
+#include <ranges>
+#include <stdexcept>
+
 #include "shift/core/Clock.hpp"
+
+#include "shift/core/Component.hpp"
+#include "shift/core/TimeUpdater.hpp"
+
+namespace
+{
+
+// todo: add get_component<T>()?
+auto get_updater(const shift::Clock& clock) -> shift::TimeUpdater*
+{
+    auto pipeline = clock.components()
+        | std::views::transform(
+                        [](const std::unique_ptr<shift::Component>& component)
+                        { return component.get(); })
+        | std::views::filter([](const shift::Component* component)
+                             { return component != nullptr; });
+    auto found = std::ranges::find_if(
+        pipeline,
+        [](shift::Component* component)
+        { return dynamic_cast<shift::TimeUpdater*>(component) != nullptr; });
+    return found != pipeline.end() ? dynamic_cast<shift::TimeUpdater*>(*found)
+                                   : nullptr;
+}
+
+}  // namespace
 
 namespace shift
 {
 
 auto Clock::tick() -> TickFrame
 {
-    // todo: get time update component and update time
+    // todo: make TimeUpdater a proper member of this class since it's required?
+    auto* updater = get_updater(*this);
+    if (updater == nullptr) {
+        // todo: create shift::Exception?
+        throw std::runtime_error("cannot tick without updater");
+    }
+
+    const auto next_time = updater->next_time();
+    m_delta = next_time - m_time;
+    m_time = next_time;
+
     return TickFrame{.time = m_time, .delta = m_delta};
 }
 
--- test/source/core/Clock_test.cpp
+++ test/source/core/Clock_test.cpp
@@ -3,7 +3,6 @@
 #include "shift/core/Clock.hpp"
 
 #include "catch2/catch_test_macros.hpp"
-#include "shift/core/TimeTypes.hpp"
 
 TEST_CASE("Clock type", "[core][Clock]")
 {
@@ -12,11 +11,5 @@ TEST_CASE("Clock type", "[core][Clock]")
 
 TEST_CASE("Clock tick no updater", "[core][Clock]")
 {
-    auto clock = shift::Clock{};
-    const auto frame = clock.tick();
-    constexpr auto zero_time = shift::time::Microseconds{};
-    CHECK(frame.time == zero_time);
-    CHECK(frame.delta == zero_time);
-    CHECK(clock.time() == zero_time);
-    CHECK(clock.delta() == zero_time);
+    CHECK_THROWS(shift::Clock{}.tick());
 }
