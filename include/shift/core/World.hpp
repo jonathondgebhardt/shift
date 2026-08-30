080cac9e0dff0aa521410cdea43de9a02c2ce41d
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-08-30 15:49:40 -0400

    feat: add World

---
+++ include/shift/core/World.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <optional>
+#include <span>
+#include <vector>
+
+#include "shift/core/Entity.hpp"
+#include "shift/core/UUID.hpp"
+#include "shift/core/shift_core_export.hpp"
+
+namespace shift
+{
+
+class SHIFT_CORE_EXPORT World
+{
+public:
+    // c++26 adds std::optional<T&>
+    using OptionalEntityReference =
+        std::optional<std::reference_wrapper<Entity>>;
+    auto find_entity(UUID uuid) -> OptionalEntityReference;
+    auto find_entity(std::string_view name) -> OptionalEntityReference;
+
+    auto entities() const -> std::span<const Entity> { return m_entities; }
+
+    auto add_entity(Entity entity) -> void;
+
+    auto remove_entity(UUID uuid) -> bool;
+    auto remove_entity(std::string_view name) -> bool;
+
+private:
+    std::vector<Entity> m_entities;
+};
+
+}  // namespace shift
---
+++ source/core/World.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <functional>
+#include <optional>
+#include <string_view>
+#include <utility>
+#include <vector>
+
+#include "shift/core/World.hpp"
+
+#include "shift/core/Entity.hpp"
+#include "shift/core/UUID.hpp"
+
+namespace shift
+{
+
+auto World::find_entity(UUID uuid) -> OptionalEntityReference
+{
+    const auto found = std::ranges::find_if(m_entities,
+                                            [&](const Entity& entity)
+                                            { return entity.uuid() == uuid; });
+    return found != m_entities.end() ? std::optional{std::ref(*found)}
+                                     : std::nullopt;
+}
+
+auto World::find_entity(std::string_view name) -> OptionalEntityReference
+{
+    const auto found = std::ranges::find_if(m_entities,
+                                            [&](const Entity& entity)
+                                            { return entity.name() == name; });
+    return found != m_entities.end() ? std::optional{std::ref(*found)}
+                                     : std::nullopt;
+}
+
+auto World::add_entity(Entity entity) -> void
+{
+    m_entities.push_back(std::move(entity));
+}
+
+auto World::remove_entity(UUID uuid) -> bool
+{
+    return std::erase_if(m_entities,
+                         [&](const Entity& entity)
+                         { return entity.uuid() == uuid; })
+        > 0;
+}
+
+auto World::remove_entity(std::string_view name) -> bool
+{
+    return std::erase_if(m_entities,
+                         [&](const Entity& entity)
+                         { return entity.name() == name; })
+        > 0;
+}
+
+}  // namespace shift
