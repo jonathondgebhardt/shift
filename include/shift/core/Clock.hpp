84227092536abf39c48eb86e67ecde2ede9bfbe1
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-08-30 11:46:04 -0400

    refactor: Clock is no longer an Entity

--- include/shift/core/Clock.hpp
+++ include/shift/core/Clock.hpp
@@ -1,17 +1,23 @@
 #pragma once
 
-#include "shift/core/Object.hpp"
+#include <memory>
+
 #include "shift/core/TimeConvert.hpp"
 #include "shift/core/TimeTypes.hpp"
+#include "shift/core/TimeUpdater.hpp"
 #include "shift/core/shift_core_export.hpp"
 
 namespace shift
 {
 
-class SHIFT_CORE_EXPORT Clock : public Object
+class SHIFT_CORE_EXPORT Clock
 {
 public:
-    auto type() const -> const char* override { return "Clock"; }
+    auto type() const -> const char* { return "Clock"; }
+
+    auto set_updater(std::unique_ptr<TimeUpdater> updater);
+
+    auto updater() const -> TimeUpdater* { return m_updater.get(); }
 
     // todo: return m_time and m_delta?
     struct TickFrame
@@ -35,6 +41,7 @@ public:
     }
 
 private:
+    std::unique_ptr<TimeUpdater> m_updater;
     time::Microseconds m_time{};
     time::Microseconds m_delta{};
 };
--- source/core/Clock.cpp
+++ source/core/Clock.cpp
@@ -1,43 +1,23 @@
-#include <algorithm>
 #include <memory>
-#include <ranges>
 #include <stdexcept>
+#include <utility>
 
 #include "shift/core/Clock.hpp"
 
-#include "shift/core/Component.hpp"
 #include "shift/core/TimeUpdater.hpp"
-
-namespace
-{
-
-// todo: add get_component<T>()?
-auto get_updater(const shift::Clock& clock) -> shift::TimeUpdater*
-{
-    auto pipeline = clock.components()
-        | std::views::transform(
-                        [](const std::unique_ptr<shift::Component>& component)
-                        { return component.get(); })
-        | std::views::filter([](const shift::Component* component)
-                             { return component != nullptr; });
-    auto found = std::ranges::find_if(
-        pipeline,
-        [](shift::Component* component)
-        { return dynamic_cast<shift::TimeUpdater*>(component) != nullptr; });
-    return found != pipeline.end() ? dynamic_cast<shift::TimeUpdater*>(*found)
-                                   : nullptr;
-}
-
-}  // namespace
 
 namespace shift
 {
 
+auto Clock::set_updater(std::unique_ptr<TimeUpdater> updater)
+{
+    m_updater = std::move(updater);
+}
+
 auto Clock::tick() -> TickFrame
 {
     // todo: make TimeUpdater a proper member of this class since it's required?
-    auto* updater = get_updater(*this);
-    if (updater == nullptr) {
+    if (m_updater == nullptr) {
         // todo: create shift::Exception?
         throw std::runtime_error("cannot tick without updater");
     }
@@ -46,7 +26,7 @@ auto Clock::tick() -> TickFrame
     // determine the next time based on the simulations that are being run?
     // the current interface assumes the time updater is completely
     // self-contained.
-    const auto next_time = updater->next_time();
+    const auto next_time = m_updater->next_time();
     m_delta = next_time - m_time;
     m_time = next_time;
 
