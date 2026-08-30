3119defb537d013739e26cc1b50a1fb8bb3b9b71
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-08-30 15:00:13 -0400

    test: add SimulationRunner test

---
+++ test/source/core/SimulationRunner_test.cpp
@@ -0,0 +1,27 @@
+#include "shift/core/Simulation.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "shift/core/Clock.hpp"
+#include "shift/core/FixedTimeUpdater.hpp"
+#include "shift/core/SimulationRunner.hpp"
+#include "shift/core/TimeTypes.hpp"
+
+TEST_CASE("SimulationRunner run", "[core][SimulationRunner]")
+{
+    const auto delta = shift::time::Microseconds{10};
+    auto updater =
+        std::make_unique<shift::FixedTimeUpdater<shift::time::Microseconds>>(
+            delta);
+    auto runner =
+        shift::SimulationRunner{shift::Simulation{}, std::move(updater)};
+
+    auto& clock = runner.simulation().clock();
+    REQUIRE(clock.time() == shift::time::Microseconds{});
+    REQUIRE(clock.delta() == shift::time::Microseconds{});
+
+    runner.run();
+
+    CHECK(clock.time() == delta);
+    CHECK(clock.delta() == delta);
+}
