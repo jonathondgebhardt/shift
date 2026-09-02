888160a76b4f52249d0c41dd598129359fbe76a2
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-09-01 20:54:24 -0400

    feat: further constrain to stringable trait

--- include/shift/telemetry/TelemetryTraits.hpp
+++ include/shift/telemetry/TelemetryTraits.hpp
@@ -11,7 +11,16 @@ namespace shift::telemetry
 {
 
 template<typename T>
-struct TelemetryTraits
+struct TelemetryTraits;
+
+template<typename T>
+concept ToStringable = requires(T value) {
+    { std::to_string(value) } -> std::same_as<std::string>;
+};
+
+template<typename T>
+    requires ToStringable<T>
+struct TelemetryTraits<T>
 {
     static auto to_string(const T& value) -> std::string
     {
