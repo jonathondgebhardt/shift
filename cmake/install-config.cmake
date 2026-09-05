eaba4efbcba7d7c0d2a11f13070eaefb950422a4
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-09-04 20:07:11 -0400

    chore: update install rules

--- cmake/install-config.cmake
+++ cmake/install-config.cmake
@@ -1,7 +1,7 @@
 set(shift_FOUND YES)
 
 include(CMakeFindDependencyMacro)
-find_dependency(fmt)
+find_dependency(spdlog)
 
 if(shift_FOUND)
   include("${CMAKE_CURRENT_LIST_DIR}/shiftTargets.cmake")
