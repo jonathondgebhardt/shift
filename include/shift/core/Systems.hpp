156fe4e456c1d8dd2110460bff684b67bf96f162
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-09-06 11:22:13 -0400

    feat: add Systems

---
+++ include/shift/core/Systems.hpp
@@ -0,0 +1,29 @@
+#include <memory>
+#include <vector>
+
+#include "shift/core/System.hpp"
+#include "shift/core/shift_core_export.hpp"
+
+namespace shift
+{
+
+class SHIFT_CORE_EXPORT Systems
+{
+public:
+    auto add_system(std::unique_ptr<System> system) -> void;
+
+    auto remove_system(SystemUID uid) -> bool;
+
+    auto find_system(SystemUID uid) -> System*;
+
+    auto systems() const -> std::span<const std::unique_ptr<System>>
+    {
+        return m_systems;
+    }
+
+private:
+    SHIFT_SUPPRESS_C4251
+    std::vector<std::unique_ptr<System>> m_systems;
+};
+
+}  // namespace shift
---
+++ source/core/Systems.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "shift/core/Systems.hpp"
+
+#include "shift/core/System.hpp"
+#include "shift/logger/Log.hpp"
+
+namespace shift
+{
+
+auto Systems::add_system(std::unique_ptr<System> system) -> void
+{
+    if (!system) {
+        shift::log::warning().append("refusing to add null system");
+        return;
+    }
+
+    static auto uid = SystemUID{0};
+    system->set_uid(++uid);
+    m_systems.push_back(std::move(system));
+}
+
+auto Systems::remove_system(SystemUID uid) -> bool
+{
+    return std::erase_if(m_systems,
+                         [&](const std::unique_ptr<System>& system)
+                         { return system->uid() == uid; })
+        > 0;
+}
+
+auto Systems::find_system(SystemUID uid) -> System*
+{
+    const auto found =
+        std::ranges::find_if(m_systems,
+                             [&](const std::unique_ptr<System>& system)
+                             { return system->uid() == uid; });
+    return found != m_systems.end() ? found->get() : nullptr;
+}
+
+}  // namespace shift
