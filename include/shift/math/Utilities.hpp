e77d1b7590c1d8b8cf4ae23cb3732fa210b1c9fc
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-09-07 20:47:56 -0400

    feat: expose epsilon

--- include/shift/math/Utilities.hpp
+++ include/shift/math/Utilities.hpp
@@ -4,14 +4,18 @@
 #include <limits>
 #include <type_traits>
 
+#include "shift/math/shift_math_export.hpp"
+
 namespace shift::math
 {
 
 template<typename T>
     requires std::is_floating_point_v<T>
-constexpr auto floating_point_eq(const T lhs, const T rhs) -> auto
+SHIFT_MATH_EXPORT constexpr auto floating_point_eq(
+    const T lhs, const T rhs, T epsilon = std::numeric_limits<T>::epsilon())
+    -> auto
 {
-    return std::abs(lhs - rhs) < std::numeric_limits<T>::epsilon();
+    return std::abs(lhs - rhs) < epsilon;
 }
 
 }  // namespace shift::math
