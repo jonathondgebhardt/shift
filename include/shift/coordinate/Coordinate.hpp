15554f55de2297c135816ef45bcaf5352e91f2a4
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-09-10 17:27:36 -0400

    feat: add coordinate systems

---
+++ include/shift/coordinate/Coordinate.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <format>
+
+namespace shift::coordinate
+{
+
+struct EcefPosition
+{
+    double x{};
+    double y{};
+    double z{};
+
+    auto to_string() const -> std::string
+    {
+        return std::format("({}, {}, {})", x, y, z);
+    }
+};
+
+struct EcefVector
+{
+    double x{};
+    double y{};
+    double z{};
+
+    auto to_string() const -> std::string
+    {
+        return std::format("({}, {}, {})", x, y, z);
+    }
+};
+
+struct GeodeticPosition
+{
+    double latitude{};
+    double longitude{};
+    double altitude{};
+
+    auto to_string() const -> std::string
+    {
+        return std::format("({}, {}, {})", latitude, longitude, altitude);
+    }
+};
+
+struct EnuPosition
+{
+    double east{};
+    double north{};
+    double up{};
+
+    auto to_string() const -> std::string
+    {
+        return std::format("({}, {}, {})", east, north, up);
+    }
+};
+
+struct EnuVector
+{
+    double east{};
+    double north{};
+    double up{};
+
+    auto to_string() const -> std::string
+    {
+        return std::format("({}, {}, {})", east, north, up);
+    }
+};
+
+struct EnuFrame
+{
+    GeodeticPosition origin;
+};
+
+}  // namespace shift::coordinate
