edd0bf6bb14edd7990008f19e1f8bb3b6ddfaba6
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-09-10 17:27:13 -0400

    feat: add GeographicLib

--- cmake/install-config.cmake
+++ cmake/install-config.cmake
@@ -2,6 +2,7 @@ set(shift_FOUND YES)
 
 include(CMakeFindDependencyMacro)
 find_dependency(spdlog)
+find_dependency(GeographicLib)
 
 if(shift_FOUND)
   include("${CMAKE_CURRENT_LIST_DIR}/shiftTargets.cmake")
