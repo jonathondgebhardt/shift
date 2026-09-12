74bf9e03498c4874830d3500ba8234c2c09a8967
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-09-12 09:13:56 -0400

    chore: add missing include

--- test/source/telemetry/TelemetryValue_test.cpp
+++ test/source/telemetry/TelemetryValue_test.cpp
@@ -3,6 +3,7 @@
 #include "shift/telemetry/TelemetryValue.hpp"
 
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 #include <catch2/matchers/catch_matchers_string.hpp>
 
