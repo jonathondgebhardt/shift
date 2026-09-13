64229c8a38c13cf4c67bd124f16dd37fc5654b59
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-09-13 08:03:17 -0400

    refactor: move helper concept into separate namespace

--- include/shift/time/TimeTypes.hpp
+++ include/shift/time/TimeTypes.hpp
@@ -5,6 +5,14 @@
 namespace shift::time
 {
 
+namespace detail
+{
+
+template<typename T, typename... U>
+concept is_any_of = (std::same_as<T, U> || ...);
+
+}  // namespace detail
+
 using Microseconds = std::chrono::microseconds;
 using Milliseconds = std::chrono::milliseconds;
 using Seconds = std::chrono::seconds;
@@ -12,13 +20,9 @@ using Minutes = std::chrono::minutes;
 using Hours = std::chrono::hours;
 using Days = std::chrono::days;
 
-// todo: hide this helper
-template<typename T, typename... U>
-concept is_any_of = (std::same_as<T, U> || ...);
-
 // todo: relax constraint to T being std::chrono::duration?
 template<typename T>
-concept TimeType =
+concept TimeType = detail::
     is_any_of<T, Microseconds, Milliseconds, Seconds, Minutes, Hours, Days>;
 
 // todo: constrain U to std::chrono::duration?
@@ -29,9 +33,9 @@ constexpr auto time_cast(U time) -> T
 }
 
 template<typename T>
-concept ClockType = is_any_of<T,
-                              std::chrono::high_resolution_clock,
-                              std::chrono::steady_clock,
-                              std::chrono::system_clock>;
+concept ClockType = detail::is_any_of<T,
+                                      std::chrono::high_resolution_clock,
+                                      std::chrono::steady_clock,
+                                      std::chrono::system_clock>;
 
 }  // namespace shift::time
