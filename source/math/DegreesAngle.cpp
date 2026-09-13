2f908954b269addd190a731a29c701356f19e298
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-09-13 09:14:21 -0400

    feat: wrap angle between [0, 360)

--- source/math/DegreesAngle.cpp
+++ source/math/DegreesAngle.cpp
@@ -2,19 +2,44 @@
 
 #include "shift/math/DegreesAngle.hpp"
 
-// todo: throw or do math to put angle between [0, 360)?
+namespace
+{
+
+// todo: move this into math
+auto fix_up(float angle_degrees) -> float
+{
+    constexpr auto lower_bound = 0.0f;
+    constexpr auto upper_bound = 359.0f;
+
+    if (angle_degrees >= lower_bound && angle_degrees <= upper_bound) {
+        return angle_degrees;
+    }
+
+    // put angle_degrees within [0, 360)
+    constexpr auto full_circle = 360.0f;
+    while (angle_degrees > upper_bound) {
+        angle_degrees -= full_circle;
+    }
+    while (angle_degrees < lower_bound) {
+        angle_degrees += full_circle;
+    }
+
+    return angle_degrees;
+}
+
+}  // namespace
 
 namespace shift::math
 {
 
 DegreesAngle::DegreesAngle(float angle)
-    : m_angle{angle}
+    : m_angle{fix_up(angle)}
 {
 }
 
 auto DegreesAngle::set_angle(float angle) -> void
 {
-    m_angle = angle;
+    m_angle = fix_up(angle);
 }
 
 auto DegreesAngle::to_string() const -> std::string
--- test/source/math/DegreesAngle_test.cpp
+++ test/source/math/DegreesAngle_test.cpp
@@ -5,19 +5,58 @@
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 #include <catch2/matchers/catch_matchers_string.hpp>
 
-TEST_CASE("Angle zero initialized", "[math][Angle]")
+TEST_CASE("DegreesAngle zero initialized", "[math][DegreesAngle]")
 {
     constexpr auto angle = shift::math::DegreesAngle{};
     CHECK_THAT(angle.angle(), Catch::Matchers::WithinRel(0.0f));
 }
 
-TEST_CASE("Angle parameterized ctor", "[math][Angle]")
+TEST_CASE("DegreesAngle parameterized ctor", "[math][DegreesAngle]")
 {
-    const auto angle = shift::math::DegreesAngle{1.0f};
-    CHECK_THAT(angle.angle(), Catch::Matchers::WithinRel(1.0f));
+    SECTION("within bounds")
+    {
+        const auto angle = shift::math::DegreesAngle{1.0f};
+        CHECK_THAT(angle.angle(), Catch::Matchers::WithinRel(1.0f));
+    }
+
+    SECTION("above bounds")
+    {
+        const auto angle = shift::math::DegreesAngle{361.0f};
+        CHECK_THAT(angle.angle(), Catch::Matchers::WithinRel(1.0f));
+    }
+
+    SECTION("below bounds")
+    {
+        const auto angle = shift::math::DegreesAngle{-1.0f};
+        CHECK_THAT(angle.angle(), Catch::Matchers::WithinRel(359.0f));
+    }
 }
 
-TEST_CASE("Angle to_string", "[math][Angle]")
+TEST_CASE("DegreesAngle set_angle", "[math][DegreesAngle]")
+{
+    SECTION("within bounds")
+    {
+        auto angle = shift::math::DegreesAngle{};
+        angle.set_angle(1.0f);
+        CHECK_THAT(angle.angle(), Catch::Matchers::WithinRel(1.0f));
+    }
+
+    SECTION("above bounds")
+    {
+        auto angle = shift::math::DegreesAngle{};
+        angle.set_angle(361.0f);
+        CHECK_THAT(angle.angle(), Catch::Matchers::WithinRel(1.0f));
+    }
+
+    SECTION("below bounds")
+    {
+        auto angle = shift::math::DegreesAngle{};
+        angle.set_angle(-1.0f);
+        CHECK_THAT(angle.angle(), Catch::Matchers::WithinRel(359.0f));
+    }
+}
+
+TEST_CASE("DegreesAngle to_string", "[math][DegreesAngle]")
 {
     const auto angle = shift::math::DegreesAngle{1.0};
     CHECK_THAT(angle.to_string(), Catch::Matchers::StartsWith("1.0"));
