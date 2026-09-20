0c97cb41922536bc96c16dbf6536311dbb358ad5
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-09-19 20:58:05 -0400

    test: add DataDefinition tests

---
+++ test/source/telemetry/DataDefinition_test.cpp
@@ -0,0 +1,30 @@
+#include "shift/telemetry/DataDefinition.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers.hpp>
+
+namespace
+{
+
+struct TestOwner
+{
+    int number{};
+};
+
+constexpr auto test_definition =
+    shift::telemetry::DataDefinition<TestOwner, int>{
+        "test_definition",
+        [](const TestOwner& test_owner) { return test_owner.number; }};
+
+}  // namespace
+
+TEST_CASE("DataDefinition name", "[telemetry][DataDefinition]")
+{
+    CHECK(test_definition.name() == "test_definition");
+}
+
+TEST_CASE("DataDefinition get", "[telemetry][DataDefinition]")
+{
+    constexpr auto owner = TestOwner{.number = 1337};
+    CHECK(test_definition.get(owner) == owner.number);
+}
