080a9e2953f1ace48e956b7dc5cf309d00b07b46
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-09-20 21:14:57 -0400

    test: add Telemetry test

---
+++ test/source/telemetry/Telemetry_test.cpp
@@ -0,0 +1,27 @@
+#include "shift/telemetry/Telemetry.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "shift/telemetry/TelemetryRecord.hpp"
+#include "shift/telemetry/TelemetryRecorder.hpp"
+
+TEST_CASE("Telemetry shutdown", "[telemetry][Telemetry]")
+{
+    struct TestTelemetryRecorder : shift::telemetry::TelemetryRecorder
+    {
+        auto record([[maybe_unused]] const shift::telemetry::TelemetryRecord&
+                        record) -> void override
+        {
+        }
+
+        auto shutdown() -> void override { was_shutdown = true; }
+
+        bool was_shutdown{};
+    };
+
+    auto telemetry =
+        shift::telemetry::Telemetry{std::make_unique<TestTelemetryRecorder>()};
+    telemetry.shutdown();
+    CHECK(dynamic_cast<TestTelemetryRecorder*>(telemetry.recorder())
+              ->was_shutdown);
+}
