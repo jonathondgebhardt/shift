cba8393f2b5b31b41e349c68f28c102752371ce5
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2026-09-20 20:21:26 -0400

    test: add UpdateResult tests

---
+++ test/source/core/UpdateResult_test.cpp
@@ -0,0 +1,54 @@
+#include "shift/core/UpdateResult.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "shift/time/Duration.hpp"
+#include "shift/time/SimulationTime.hpp"
+#include "shift/time/TimeTypes.hpp"
+
+TEST_CASE("UpdateResult schedule_at", "[core][UpdateResult]")
+{
+    constexpr auto time = shift::time::SimulationTime{shift::time::Seconds{1}};
+    constexpr auto update_result = shift::UpdateResult::schedule_at(time);
+    CHECK(!update_result.stopped());
+
+    const auto next_time =
+        update_result.next_time(shift::time::SimulationTime{});
+    REQUIRE(next_time);
+    // NOLINTNEXTLINE(bugprone-unchecked-optional-access)
+    CHECK(*next_time == time);
+}
+
+TEST_CASE("UpdateResult schedule_after", "[core][UpdateResult]")
+{
+    constexpr auto duration = shift::time::Duration{shift::time::Seconds{1}};
+    constexpr auto update_result =
+        shift::UpdateResult::schedule_after(duration);
+    CHECK(!update_result.stopped());
+
+    const auto next_time =
+        update_result.next_time(shift::time::SimulationTime{});
+    REQUIRE(next_time);
+    // NOLINTNEXTLINE(bugprone-unchecked-optional-access)
+    CHECK(next_time->data() == duration.data());
+}
+
+TEST_CASE("UpdateResult schedule_now", "[core][UpdateResult]")
+{
+    constexpr auto update_result = shift::UpdateResult::schedule_now();
+    CHECK(!update_result.stopped());
+
+    const auto next_time =
+        update_result.next_time(shift::time::SimulationTime{});
+    REQUIRE(next_time);
+
+    // NOLINTNEXTLINE(bugprone-unchecked-optional-access)
+    CHECK(*next_time == shift::time::SimulationTime{});
+}
+
+TEST_CASE("UpdateResult stop", "[core][UpdateResult]")
+{
+    constexpr auto update_result = shift::UpdateResult::stop();
+    CHECK(update_result.stopped());
+    CHECK(!update_result.next_time(shift::time::SimulationTime{}));
+}
